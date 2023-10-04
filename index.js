fb85f917ec40506304e6d3e0542a6467b7a402da
Sarbagya Dhaubanjar <sarbagyastha@gmail.com>
2023-10-04 18:25:16 +0545

    call endpoint

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
-import { getInput, setFailed, summary } from "@actions/core";
+import { getInput, getMultilineInput, setFailed, summary } from "@actions/core";
+import { get } from "https";
 
 try {
   const pat = getInput("git-pat");
   const repo = getInput("repo");
   const ref = getInput("ref");
-  const includes = getInput("includes");
 
   console.log(`pat: ${pat}`);
   console.log(`repo: ${repo}`);
   console.log(`ref: ${ref}`);
-  console.log(`includes: ${includes}`);
+
+  const options = {
+    headers: {},
+  };
+
+  if (pat) {
+    options.headers = {
+      Authorization: `token ${pat}`,
+    };
+  }
+
+  getMultilineInput("includes").forEach((include) => {
+    const [input, output] = include.split(":");
+
+    const fileStream = fs.createWriteStream(output);
+
+    get(
+      `https://raw.githubusercontent.com/${org}/${ref}/${input}`,
+      options,
+      (res) => {
+        res.pipe(fileStream);
+
+        fileStream.on("end", () => {
+          console.log(`Downloaded "${input}" to "${output}".`);
+        });
+      }
+    ).on("error", (err) => {
+      setFailed(err.message);
+    });
+  });
 
   summary
     .addHeading("Summary")
@@ -19,6 +48,7 @@ try {
         { data: "Result", header: true },
       ],
       ["Repo", repo],
+      ["Ref", ref],
     ])
     .write();
 } catch (error) {
